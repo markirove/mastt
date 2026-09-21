d139b9d15eb86a6bf8bac19165046ff75b8e215e
Dipu <sadhanaroy198421@gmail.com>
2026-09-21 20:44:04 +0530

    Update di.d.ts

--- playeon-bot/src/types/di.d.ts
+++ playeon-bot/src/types/di.d.ts
@@ -1,13 +1,11 @@
 import type { Collections } from '../services/mongo.js'
 import type { CacheService } from '../services/redis.js'
-import type { Logger } from '../services/logger.js'
 import type { config } from '../config.js'
 
 declare module '@mtcute/dispatcher' {
   interface DispatcherDependencies {
     db: Collections
     cache: CacheService
-    logger: Logger
     config: typeof config
   }
 }
